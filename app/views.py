fa8c5e079bd1fc20fddf826d9f1c7c9e21ab537c
Sonu64 <sonusantu64@gmail.com>
2026-01-19 16:45:13 +0530

    Learnt some Django Frontend syntax using something similar to Jinja... now showing Admin Created articles on the homepage!

--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from app.models import Article
+from django.shortcuts import render
+import random
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Welcome to the world of Django !")
+    allArticles = Article.objects.all() # Using Django ORM for the 1st time ⏰
+    return render(request, "app/home.html", {"articles":allArticles})
 
 
