58c8257a2c7996132fc5121a6e62f53ae1cd264b
Sonu64 <sonusantu64@gmail.com>
2026-01-19 18:20:30 +0530

    Learnt about model forms and class Meta.

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,8 @@
 from django import forms 
-from app.models import ARTICLE_STATUS # Already imported Article Status here, not required to define below.
+from app.models import Article
 
 
 class CreateArticleForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    status = forms.ChoiceField(choices=ARTICLE_STATUS)
-    content = forms.CharField(widget = forms.Textarea)
-    wordCount = forms.IntegerField()
-    twitterPost = forms.CharField(widget=forms.Textarea, required=False)
+    class Meta:
+        model = Article
+        fields = ("title", "content", "wordCount", "twitterPost", "status")
