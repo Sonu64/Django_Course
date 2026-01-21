7301c47a4f22d8ef5487216d8afa662e2746c3e5
Sonu64 <sonusantu64@gmail.com>
2026-01-21 10:11:52 +0530

    Learnt about Class Based Views, learnt when using CBV is better than FBV and vice-versa. Generally for same action URL, we use CBVs, but for different action URL we choose FBV to make it more clear and easy-to-understand. ex:- POST only  URLSprefer FBVs instead of CBVs.

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from app.views import home
-from app.views import createArticle
+from app.views import ArticleCreateView
 
 
 urlpatterns = [
     path("", home, name = 'home'),
-    path("articles/create/", createArticle, name = "createArticle")
+    path("articles/create/", ArticleCreateView.as_view(), name = "createArticle")
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 # from django.shortcuts import render
 from django.http import HttpResponse 
 from app.models import Article
-from django.shortcuts import render, redirect
-from app.forms import CreateArticleForm
-from django import forms
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -12,25 +12,37 @@ def home(req):
     allArticles = Article.objects.all() # Using Django ORM for the 1st time ⏰
     return render(req, "app/home.html", {"articles":allArticles})
 
-def createArticle(req):
-    if req.method == 'POST':
-        form = CreateArticleForm(req.POST)
-        if form.is_valid():
-            formData = form.cleaned_data
-            newArticle = Article(
-                title = formData['title'],
-                status = formData['status'],
-                content = formData['content'],
-                wordCount = formData['wordCount'],
-                twitterPost = formData['twitterPost']
-            )
-            newArticle.save() 
-            return redirect('home')       
+
+
+# Function Based View
+# def createArticle(req):
+#     if req.method == 'POST':
+#         form = CreateArticleForm(req.POST)
+#         if form.is_valid():
+#             formData = form.cleaned_data
+#             newArticle = Article(
+#                 title = formData['title'],
+#                 status = formData['status'],
+#                 content = formData['content'],
+#                 wordCount = formData['wordCount'],
+#                 twitterPost = formData['twitterPost']
+#             )
+#             newArticle.save() 
+#             return redirect('home')       
         
-    elif req.method == 'GET':
-        form = CreateArticleForm()
+#     elif req.method == 'GET':
+#         form = CreateArticleForm()
         
-    return render(req, 'app/create_article.html', {'form':form})
+#     return render(req, 'app/create_article.html', {'form':form})
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'app/create_article.html'
+    model = Article
+    fields = ["title", "content", "status", "wordCount", "twitterPost"]
+    success_url = reverse_lazy('home')
+    
+    
     
 
 
