5da0466b98d58cded63a7a59b86c91cb09166caf
Sonu64 <sonusantu64@gmail.com>
2026-01-21 09:31:19 +0530

    Learnt about Form Creation in a view, based on a CreateForm() function. Also learnt bout  GET and POST requests, and submitting the Form.

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from app.views import home
+from app.views import createArticle
 
 
 urlpatterns = [
-    path("", home),
+    path("", home, name = 'home'),
+    path("articles/create/", createArticle, name = "createArticle")
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,37 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
 from app.models import Article
-from django.shortcuts import render
-import random
+from django.shortcuts import render, redirect
+from app.forms import CreateArticleForm
+from django import forms
 
 
 # Create your views here.
 
-def home(request):
+def home(req):
     allArticles = Article.objects.all() # Using Django ORM for the 1st time ⏰
-    return render(request, "app/home.html", {"articles":allArticles})
+    return render(req, "app/home.html", {"articles":allArticles})
+
+def createArticle(req):
+    if req.method == 'POST':
+        form = CreateArticleForm(req.POST)
+        if form.is_valid():
+            formData = form.cleaned_data
+            newArticle = Article(
+                title = formData['title'],
+                status = formData['status'],
+                content = formData['content'],
+                wordCount = formData['wordCount'],
+                twitterPost = formData['twitterPost']
+            )
+            newArticle.save() 
+            return redirect('home')       
+        
+    elif req.method == 'GET':
+        form = CreateArticleForm()
+        
+    return render(req, 'app/create_article.html', {'form':form})
+    
+
 
 
