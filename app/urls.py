04d5b566f7d6b941fbf70c37add36f2ee87cf2f6
Sonu64 <sonusantu64@gmail.com>
2026-01-21 11:09:35 +0530

    Added other endpoints and learnt about context_variable_name and default form variable and Data Flow Sequence.

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from app.views import home
-from app.views import ArticleCreateView
+from app.views import ArticleCreateView, ArticleListView
 
 
 urlpatterns = [
-    path("", home, name = 'home'),
+    path("", ArticleListView.as_view(), name = 'home'),
     path("articles/create/", ArticleCreateView.as_view(), name = "createArticle")
 ]
--- app/views.py
+++ app/views.py
@@ -3,37 +3,14 @@ from django.http import HttpResponse
 from app.models import Article
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 # Create your views here.
 
-def home(req):
-    allArticles = Article.objects.all() # Using Django ORM for the 1st time ⏰
-    return render(req, "app/home.html", {"articles":allArticles})
-
-
-
-# Function Based View
-# def createArticle(req):
-#     if req.method == 'POST':
-#         form = CreateArticleForm(req.POST)
-#         if form.is_valid():
-#             formData = form.cleaned_data
-#             newArticle = Article(
-#                 title = formData['title'],
-#                 status = formData['status'],
-#                 content = formData['content'],
-#                 wordCount = formData['wordCount'],
-#                 twitterPost = formData['twitterPost']
-#             )
-#             newArticle.save() 
-#             return redirect('home')       
-        
-#     elif req.method == 'GET':
-#         form = CreateArticleForm()
-        
-#     return render(req, 'app/create_article.html', {'form':form})
+class ArticleListView(ListView):
+    template = 'app/home.html'
+    model = Article
+    context_object_name = "articles"
 
 
 class ArticleCreateView(CreateView):
@@ -41,6 +18,22 @@ class ArticleCreateView(CreateView):
     model = Article
     fields = ["title", "content", "status", "wordCount", "twitterPost"]
     success_url = reverse_lazy('home')
+    
+    
+class ArticleUpdateView(UpdateView):
+    template_name = 'app/update_article.html'
+    model = Article
+    fields = ["title", "content", "status", "wordCount", "twitterPost"]
+    success_url = reverse_lazy('home')
+    context_object_name = "article"
+    
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'app/delete_article.html' # id here, just contains something like 
+    # do you want to delete {{ article.title }} ?
+    model = Article
+    success_url = reverse_lazy('home')
+    context_object_name = 'article' # this contains the deleted row 
     
     
     
