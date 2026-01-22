8d32ece85f8839bdbd0d3a683900a4297b764690
Sonu64 <sonusantu64@gmail.com>
2026-01-22 19:44:17 +0530

    Added ListView, CreateView, UpdateView, DeleteView for articles. Also learnt about passing article ID as the Primary key in every route URL. Cleared up how context_object_name and form object works.

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from app.views import home
+# from app.views import home
 from app.views import ArticleCreateView, ArticleListView
+from app.views import ArticleUpdateView, ArticleDeleteView
 
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name = 'home'),
-    path("articles/create/", ArticleCreateView.as_view(), name = "createArticle")
+    path("articles/create/", ArticleCreateView.as_view(), name="createArticle"),
+    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="updateArticle"),
+    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="deleteArticle")
 ]
--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 # Create your views here.
 
 class ArticleListView(ListView):
-    template = 'app/home.html'
+    template_name = 'app/home.html'
     model = Article
     context_object_name = "articles"
 
@@ -20,17 +20,23 @@ class ArticleCreateView(CreateView):
     success_url = reverse_lazy('home')
     
     
+    
 class ArticleUpdateView(UpdateView):
+    # Primary Key = ID, fetched from Route, automatically by Django Magic !
+    # UpdateView fetches the article with that ID, pre-fills the form with its values
+    # And stores this fetched article in the context_object_name
     template_name = 'app/update_article.html'
     model = Article
     fields = ["title", "content", "status", "wordCount", "twitterPost"]
     success_url = reverse_lazy('home')
-    context_object_name = "article"
+    context_object_name = "article" # this contains the row to be updated
     
 
 class ArticleDeleteView(DeleteView):
-    template_name = 'app/delete_article.html' # id here, just contains something like 
-    # do you want to delete {{ article.title }} ?
+    # Primary Key = ID, fetched from Route, automatically by Django Magic !
+    # DeleteView deletes the article with that id if success_url hit.
+    # Else it just stores the article in an object, and not delete it if we go back.
+    template_name = 'app/delete_article.html'
     model = Article
     success_url = reverse_lazy('home')
     context_object_name = 'article' # this contains the deleted row 
