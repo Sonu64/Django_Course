7b79e13cd9b90471a540f9445b4479e6886412b0
Sonu64 <sonusantu64@gmail.com>
2026-01-22 19:50:12 +0530

    Cleaned up URLS and added articles/ to Project URL. So localhost does not exist anymore, localhost/articles does exist !

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from app.views import ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name = 'home'),
-    path("articles/create/", ArticleCreateView.as_view(), name="createArticle"),
-    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="updateArticle"),
-    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="deleteArticle")
+    path("create/", ArticleCreateView.as_view(), name="createArticle"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="updateArticle"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="deleteArticle")
 ]
