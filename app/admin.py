37adc9dd3d5e3cf5be2fa6c538715a74392cd135
Sonu64 <sonusantu64@gmail.com>
2026-01-30 13:23:11 +0530

    feat: add migration to recover or auto-create superuser with default credentials; customize UserAdmin in admin panel

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from app.models import User, Article
 # Register your models here.
 
+class CustomUserAdmin(UserAdmin):
+    # We can customize the UserAdmin here if needed, for now we will use the default settings.
+    pass
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "wordCount", "status", "createdAt", "updatedAt",) # we avoid to show the content field here
     list_filter = ("status",)
@@ -12,7 +17,7 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ("wordCount", "createdAt", "updatedAt",)
     
 
-admin.site.register(User, UserAdmin) # Here User is the default User model we created in models.py - inheriting from Djangos AbstractUser class.
+admin.site.register(User, CustomUserAdmin) # Here User is the default User model we created in models.py - inheriting from Djangos AbstractUser class.
 admin.site.register(Article, ArticleAdmin)
 
 ### ------- SYNTAX REMINDER FOR ADMIN CUSTOMIZATION -------- ###
